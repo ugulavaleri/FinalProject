03038560a898ca393a6ed7bec59b110425162d91
ugulavaleri <leri.ugulava1@gmail.com>
2023-02-18 14:17:58 +0400

    created 'caseStudies' section

--- MainPage.js
+++ MainPage.js
@@ -12,3 +12,29 @@ mobileIcon.addEventListener("click", () => {
         document.documentElement.classList.remove("scrollHide");
     }
 });
+
+//  swiper JS (Works after refresh)
+
+const mySwipper = document.getElementById("mySwipper");
+const swiperWrapper = document.getElementById("swiperWrapper");
+const swiperSlide1 = document.getElementById("swiperSlide1");
+const swiperSlide2 = document.getElementById("swiperSlide2");
+const swiperSlide3 = document.getElementById("swiperSlide3");
+const swiperScrollbar = document.getElementById("swiperScrollbar");
+
+if (window.matchMedia("(max-width: 800px)").matches) {
+    var swiper = new Swiper(".mySwiper", {
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+        },
+    });
+} else {
+    mySwipper.classList.remove("mySwiper");
+    mySwipper.classList.remove("swiper");
+    swiperWrapper.classList.remove("swiper-wrapper");
+    swiperSlide1.classList.remove("swiper-slide");
+    swiperSlide2.classList.remove("swiper-slide");
+    swiperSlide3.classList.remove("swiper-slide");
+    swiperScrollbar.classList.remove("swiper-scrollbar");
+}
