353212c6fea3e99f458f2c959d8b1d6f48be1eee
ugulavaleri <leri.ugulava1@gmail.com>
2023-02-19 03:27:25 +0400

    added frequently asked questions section

--- MainPage.js
+++ MainPage.js
@@ -14,7 +14,7 @@ mobileIcon.addEventListener("click", () => {
     }
 });
 
-//  swiper JS  -- (Works after refresh)
+//  swiper JS  -- (If you manually reduce the resolution, swiper Works after refresh page)
 
 const mySwipper = document.getElementById("mySwipper");
 const swiperWrapper = document.getElementById("swiperWrapper");
@@ -23,6 +23,8 @@ const swiperSlide2 = document.getElementById("swiperSlide2");
 const swiperSlide3 = document.getElementById("swiperSlide3");
 const swiperScrollbar = document.getElementById("swiperScrollbar");
 
+const insideSwiperBox = document.getElementById("insideSwiperBox");
+
 if (window.matchMedia("(max-width: 800px)").matches) {
     var swiper = new Swiper(".mySwiper", {
         scrollbar: {
@@ -30,6 +32,7 @@ if (window.matchMedia("(max-width: 800px)").matches) {
             hide: true,
         },
     });
+    insideSwiperBox.classList.remove("direction-column");
 } else {
     mySwipper.classList.remove("mySwiper");
     mySwipper.classList.remove("swiper");
@@ -38,4 +41,24 @@ if (window.matchMedia("(max-width: 800px)").matches) {
     swiperSlide2.classList.remove("swiper-slide");
     swiperSlide3.classList.remove("swiper-slide");
     swiperScrollbar.classList.remove("swiper-scrollbar");
+}
+
+/*  Frequently Asked question list   */
+/*  If you manually reduce the resolution, you need to restart page for swiper. (another time click list items)   */
+
+const questionItem = document.getElementsByClassName("questionItem");
+const afterClickShow = document.getElementsByClassName("afterClickShow");
+const dropDownIcon = document.getElementsByClassName("dropDownIcon");
+
+for (let index = 0; index < afterClickShow.length; index++) {
+    const element = questionItem[index];
+    element.addEventListener("click", () => {
+        afterClickShow[index].classList.toggle("showText");
+        if (afterClickShow[index].classList.contains("showText")) {
+            dropDownIcon[index].style.transform = "rotate(90deg)";
+        } else {
+            dropDownIcon[index].style.transform = "rotate(0deg)";
+        }
+        afterClickShow[index].classList.toggle("hideText");
+    });
 }
